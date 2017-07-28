4e3ea4ab091b2c2ef3c51332debc2e35eb1bb76e
Ubuntu <ubuntu@ip-172-31-3-94.us-west-2.compute.internal>
2017-07-28 05:57:16 +0000

    second

--- shop/shop/settings.py
+++ shop/shop/settings.py
@@ -26,9 +26,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'lo$i&wreyh8*=(@l@g-68vmdjjqieckox+!x5**9alpl7&7axj'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['ec2-52-35-3-117.us-west-2.compute.amazonaws.com']
+ALLOWED_HOSTS = ['52.35.3.117','ec2-52-35-3-117.us-west-2.compute.amazonaws.com']
 
 
 # Application definition
@@ -146,7 +146,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = location('public/static')
+#STATIC_URL = os.path.join(BASE_DIR, 'static/')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+#STATIC_ROOT = location('/home/ubuntu/django/shop/static')
+
 
 
 HAYSTACK_CONNECTIONS = {
